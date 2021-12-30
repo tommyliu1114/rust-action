0b916f6ca3167bb80502714353f02d70df9e7bf7
liusen <liusen@miliantech.com>
2021-12-30 22:55:24 +0800

    fix

--- chap2guessing/src/main.rs
+++ chap2guessing/src/main.rs
@@ -1,9 +1,11 @@
 use std::io;
-
+use rand::Rng;
 fn main() {
     println!("guess!");
+    let secret = rand::thread_rng().gen_range(1,89);
+    println!("your gduess is : {} ",secret);
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("read line error");
-    println!("your guess is : {} ",guess);
+    println!("your gduess is : {} ",guess);
 
 }
