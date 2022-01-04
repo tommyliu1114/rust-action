65e7c18c52c323bf76694f0ae38457c89d05fe92
liusen <liusen@miliantech.com>
2022-01-04 23:39:37 +0800

    fix

--- chap4string/src/main.rs
+++ chap4string/src/main.rs
@@ -9,4 +9,6 @@ fn main() {
     //clone 仅仅针对heap上数据，栈上的数据stack不需要，廉价的创造
     let s3 = s1.clone();
     println!("s1 is {}, s3 is {}",s1,s3);
+    //copy train 和 drop trait互斥；
+    //copy-trait的类型：整形，浮点，bool，char，字段都为copy的tuple
 }
