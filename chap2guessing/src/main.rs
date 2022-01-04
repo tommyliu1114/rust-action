39ea7165fb97f87de4c8d2a253f0fe11ed561c76
liusen <liusen@miliantech.com>
2022-01-04 12:52:11 +0800

    fix

--- chap2guessing/src/main.rs
+++ chap2guessing/src/main.rs
@@ -5,14 +5,21 @@ fn main() {
     println!("guess!");
     let secret = rand::thread_rng().gen_range(1,89);
     println!("your gduess is : {} ",secret);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("read line error");
-    println!("your gduess is : {} ",guess);
-    let guess_n :u32 = guess.trim().parse().expect("please input a number");
-    match guess_n.cmp(&secret){
-        Ordering::Less => println!("too small"),
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("you got the number : {} ",guess)
+    
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("read line error");
+        println!("your gduess is : {} ",guess);
+        let guess_n :u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {println!("input wrong");continue} 
+        };
+        match guess_n.cmp(&secret){
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {println!("you got the number : {} ",guess);break;}
+        }
     }
+ 
 
 }
