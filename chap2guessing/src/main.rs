14f536c418326871fdf20c9eec0d391c8f545ab2
liusen <liusen@miliantech.com>
2022-01-04 12:15:30 +0800

    fix

--- chap2guessing/src/main.rs
+++ chap2guessing/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("guess!");
     let secret = rand::thread_rng().gen_range(1,89);
@@ -7,5 +8,11 @@ fn main() {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("read line error");
     println!("your gduess is : {} ",guess);
+    let guess_n :u32 = guess.trim().parse().expect("please input a number");
+    match guess_n.cmp(&secret){
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("you got the number : {} ",guess)
+    }
 
 }
