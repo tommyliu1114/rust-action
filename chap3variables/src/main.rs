8383c2d64150f1427fb7ba4d5356c06b5072300f
liusen <liusen@miliantech.com>
2022-01-04 22:10:20 +0800

    fix

--- chap3variables/src/main.rs
+++ chap3variables/src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     println!("Hello, world!");
     let x = 5;
     println!("the value of x is {} ",x);
+    let mut y = 6;
+    println!("the value of y is {}",y);
+    y = 100 ;
+    println!("the value of y is {}",y);
+    
 }
