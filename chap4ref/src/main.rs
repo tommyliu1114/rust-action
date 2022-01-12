ba03d4652c05eda531f3ee8632ba14e3a53d809c
liusen <liusen@miliantech.com>
2022-01-12 12:49:54 +0800

    fix

--- chap4ref/src/main.rs
+++ chap4ref/src/main.rs
@@ -2,7 +2,15 @@ fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
     println!("the length of {} is {}.",s1,len);
+    let mut s2 = String::from("hi,");
+    let s2_len = mut_calculate_length(& mut s2);
+    println!("the length of {} is {}",s2,s2_len);
 }
 fn calculate_length(s:&String) -> usize {
+    s.len()
+}
+
+fn mut_calculate_length(s:& mut String) -> usize {
+    s.push_str("lewis");
     s.len()
 }
