c860bf9eee04ffec824f7d79bdff6a4a23691250
liusen <liusen@miliantech.com>
2022-01-29 20:28:59 +0800

    fix

--- chap7crate/src/lib.rs
+++ chap7crate/src/lib.rs
@@ -1,7 +1,7 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist(){}
-        fn seat_at_table(){}
+    pub mod hosting {
+        pub fn add_to_waitlist(){}
+        pub fn seat_at_table(){}
     }
 
     mod serving {
@@ -9,4 +9,25 @@ mod front_of_house {
         fn server_order(){}
         fn take_payment(){}
     }
+
+}
+
+pub fn eat_at_restaurant(){
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::seat_at_table();
+}
+
+fn server_order() {}
+
+mod back_of_house {
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String ,  
+    }
+    fn fix_incorrect_order(){
+        cook_order();
+        super::server_order();
+    }
+    fn cook_order(){}
 }
