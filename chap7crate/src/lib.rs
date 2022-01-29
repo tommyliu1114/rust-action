64d4d7be4743e547cdbda15ec87babf09968d517
liusen <liusen@miliantech.com>
2022-01-29 20:09:16 +0800

    fix

---
+++ chap7crate/src/lib.rs
@@ -0,0 +1,12 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist(){}
+        fn seat_at_table(){}
+    }
+
+    mod serving {
+        fn taker_order(){}
+        fn server_order(){}
+        fn take_payment(){}
+    }
+}
