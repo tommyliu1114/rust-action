3d313a64c952ea4826a32ab3c248a96cfba5b9f8
liusen <liusen@miliantech.com>
2022-01-29 19:49:22 +0800

    fix

--- chap6match/src/main.rs
+++ chap6match/src/main.rs
@@ -1,15 +1,37 @@
 fn main() {
     println!("Hello, world!");
-    let cc = value_in_cents(Coin::Dime);
+    let cc = value_in_cents(Coin::Quarter(UsState::Alaska));
     println!("the value is {} ",cc);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    let v = 0u8;
+    match v {
+        1 => println!("one"),
+        _ => println!("other")
+    }
+    //if let 只针对某一种模式
+    let v1 = Some(12u8);
+    if let Some(3) = v1 {
+        println!("three")
+    }else {
+        println!("others")
+    }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Hawaiwa,
+    California,
+}
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -17,6 +39,18 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 2,
         Coin::Dime => 10,
-        Coin::Quarter => 23,
+        Coin::Quarter(gstate) => {
+            println!("in us {:?} ",gstate);
+            5
+        } 
+    }
+}
+
+// option<T> some
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
     }
 }
