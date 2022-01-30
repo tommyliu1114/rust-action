19da48ba95f0d8d699602d2a77c44a9185528e58
liusen <liusen@miliantech.com>
2022-01-30 10:46:29 +0800

    fix

--- chap8vec/src/main.rs
+++ chap8vec/src/main.rs
@@ -1,3 +1,38 @@
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut v: Vec<i32> = Vec::new();
+    let v1  = vec![1,2,3];
+    v.push(32);
+    v.push(25);
+    println!("Hello, world!, {}",v1[0]);
+    // yin yong
+    match v.get(2) {
+        Some(kk) => println!("the third is {} ",kk),
+        None => println!("no value")
+    }
+    for i in &v1 {
+        println!("the number is {}",i);
+    }
+    for kk in &mut v {
+        *kk = *kk + 1;
+        println!("the mut is {}",kk);
+    }
+
+    let row = vec![
+        SpreadsheetCell::Float(3.2),
+        SpreadsheetCell::Int(31),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+    
 }
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+
+
