ded46e31314816fed5227b7b3f616ae78fc4d6ab
liusen <liusen@miliantech.com>
2022-01-30 11:49:53 +0800

    fix

--- chap8hash/src/main.rs
+++ chap8hash/src/main.rs
@@ -4,5 +4,15 @@ fn main() {
     let mut scores = HashMap::new();
     scores.insert(String::from("hello world"), 23);
     scores.insert(String::from("nickname is nil"), 100);
-    
+    let teams = vec![String::from("blue"),String::from("yellow")];
+    let initial_scoers = vec![10,23];
+    let nscores: HashMap<_,_> = 
+    teams.iter().zip(initial_scoers.iter()).collect();
+    // 所有权
+    let blue = String::from("blued");
+    match nscores.get(&blue) {
+        Some(s) => println!("{}" ,s),
+        None => println!("team not exists")
+
+    }
 }
