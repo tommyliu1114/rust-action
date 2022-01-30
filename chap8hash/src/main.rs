37d3fb365d840ae2fc1924109d499a1e4bd00b98
liusen <liusen@miliantech.com>
2022-01-30 11:32:54 +0800

    fix

---
+++ chap8hash/src/main.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+fn main() {
+    println!("Hello, world!");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("hello world"), 23);
+    scores.insert(String::from("nickname is nil"), 100);
+    
+}
