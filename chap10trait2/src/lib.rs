9b7d7838df8e68eb256f8b417d02ddd453e4a306
liusen <liusen@miliantech.com>
2022-02-15 22:18:41 +0800

    fix

--- chap10trait2/src/lib.rs
+++ chap10trait2/src/lib.rs
@@ -4,3 +4,16 @@ pub trait Summary {
 }
 
 
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} , by {} ({})",self.headline,self.author,self.location)
+    }
+}
--- chap10trait2/src/main.rs
+++ chap10trait2/src/main.rs
@@ -1,5 +1,6 @@
 use chap10trait2::Summary;
 use std::fmt::Display;
+use chap10trait2::NewsArticle;
 fn main() {
     println!("Hello, world!");
 }
@@ -12,4 +13,23 @@ pub fn notify<T: Summary>(item: T) {
 
 pub fn notify1(item1: impl Summary + Display){
     println!("breaking news! {}",item1.summarize())
+}
+
+//trait where 
+
+pub fn  notify2<T,U>(a: T,b: U) -> String 
+where T: Summary + Display,
+U: Summary{
+    println!("breaking news! {}, b is : {} ",a.summarize(),b.summarize());
+}
+
+//traint as return 
+
+pub fn notify4(s: &str) -> impl Summary {
+    NewsArticle {
+        headline: String::from("peguins win the stanley"),
+        content: String::from("he once agin are the best hockey team in the nhl "),
+        author: String::from("iceburgh"),
+        location: String::from("pa,usa"),
+    }
 }
